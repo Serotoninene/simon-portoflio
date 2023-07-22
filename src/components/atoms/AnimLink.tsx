d97e2fa8c999f0abf3dc8c37a8cbdaf11bf09536
Serotoninene <pujol.alexandre@hotmail.fr>
2023-07-22 16:28:12 +0200

    setup the animlink to separate each word and letter in its own span

--- src/components/atoms/AnimLink.tsx
+++ src/components/atoms/AnimLink.tsx
@@ -1,15 +1,45 @@
-import Link from "next/link";
+"use client";
+
 import React from "react";
+import Link from "next/link";
+
+import { motion } from "framer-motion";
 
 type Props = {
   children: React.ReactNode;
   href?: string;
 };
 
+const containerAnim = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.05,
+    },
+  },
+  exit: {
+    transition: {
+      staggerChildren: 0.05,
+    },
+  },
+};
+
+const letterAnim = {
+
 export const AnimLink = ({ children, href = "/" }: Props) => {
+  const words = children?.toString().split(" ");
+
   return (
     <Link href={href} className="cursor-pointer">
-      {children}
+      {words?.map((word) => (
+        <span key={word}>
+          {[...word].map((letter, idx) => (
+            <motion.span key={letter + idx} className="inline-block">
+              {letter}
+            </motion.span>
+          ))}{" "}
+        </span>
+      ))}
     </Link>
   );
 };
