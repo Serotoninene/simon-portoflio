99acef1eea334f825e6691993bbf0b5ea1ef62fa
Serotoninene <pujol.alexandre@hotmail.fr>
2023-07-23 20:57:59 +0200

    tracking the scroll direction on the navbar

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 
 import { Container } from "../molecules/Container";
 import { AnimLink } from "../atoms";
 
 type Props = {};
 
+// [] the navbar disappears when scrolling down
+// [] the navbar appears when scrolling up
+// [] the animation doesn't trigger when on mobile
+
 export const Navbar = (props: Props) => {
+  const [scrollDir, setScrollDir] = useState<"up" | "down">("up");
+
+  const handleWheel = (e: WheelEvent) => {
+    if (e.deltaY > 0) {
+      setScrollDir("down");
+    } else {
+      setScrollDir("up");
+    }
+  };
+
+  useEffect(() => {
+    const container = document.querySelector("body");
+    container?.addEventListener("wheel", handleWheel);
+
+    return document.removeEventListener("wheel", handleWheel);
+  }, []);
   return (
     <Container className="fixed left-0 right-0 z-10">
       <ul className="hidden sm:flex justify-end gap-14 font-semi-bold">
