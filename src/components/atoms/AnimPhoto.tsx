90fe6b10db11da50296184d52fec34b47edb7b0b
Serotoninene <pujol.alexandre@hotmail.fr>
2023-07-26 13:57:06 +0200

    expanded the transition duration

--- src/components/atoms/AnimPhoto.tsx
+++ src/components/atoms/AnimPhoto.tsx
@@ -84,7 +84,7 @@ export const AnimPhoto = ({
           onLoad={handleLoad}
           fill
           {...imageProps}
-          className={`transition-opacity duration-500 object-cover ${
+          className={`transition-opacity duration-1000 object-cover ${
             isLoaded ? "opacity-100" : "opacity-0"
           }`}
         />
