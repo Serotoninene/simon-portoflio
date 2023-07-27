23b19e7b2c368699325420792344845395274b12
Serotoninene <pujol.alexandre@hotmail.fr>
2023-07-27 14:47:37 +0200

    tried animating on exit in the AnimPhoto

--- src/components/atoms/AnimPhoto.tsx
+++ src/components/atoms/AnimPhoto.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 import { createAlt } from "@/utils/helpers";
 import { ease } from "@/utils/store";
@@ -66,29 +66,31 @@ export const AnimPhoto = ({
   }, [ref.current, isLoaded]);
 
   return (
-    <motion.div
-      ref={ref}
-      variants={variants}
-      // initial="hidden"
-      // animate="visible"
-      // exit="hidden"
-      className="h-full relative overflow-hidden "
-    >
-      <div
-        data-scroll
-        data-scroll-speed="-1.1"
-        className="relative h-full translate-y-10"
+    <AnimatePresence mode="wait">
+      <motion.div
+        ref={ref}
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        className="h-full relative overflow-hidden "
       >
-        {/* eslint-disable-next-line */}
-        <Image
-          onLoad={handleLoad}
-          fill
-          {...imageProps}
-          className={`transition-opacity duration-1000 object-cover ${
-            isLoaded ? "opacity-100" : "opacity-0"
-          }`}
-        />
-      </div>
-    </motion.div>
+        <div
+          data-scroll
+          data-scroll-speed="-1.1"
+          className="relative h-full translate-y-10"
+        >
+          {/* eslint-disable-next-line */}
+          <Image
+            onLoad={handleLoad}
+            fill
+            {...imageProps}
+            className={`transition-opacity duration-1000 object-cover ${
+              isLoaded ? "opacity-100" : "opacity-0"
+            }`}
+          />
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
