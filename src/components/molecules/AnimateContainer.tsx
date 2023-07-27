c80a89faab2f0a4bdbbda86be23b3efb0d5fac76
Serotoninene <pujol.alexandre@hotmail.fr>
2023-07-27 18:19:58 +0200

    tried animatePresence in vain

--- src/components/molecules/AnimateContainer.tsx
+++ src/components/molecules/AnimateContainer.tsx
@@ -2,60 +2,14 @@
 
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
-import barba from "@barba/core";
+
 import { useEffect } from "react";
-import { gsap } from "gsap";
 
 type AnimateProps = {
   children: React.ReactNode;
 };
 
-// export const AnimateContainer = ({ children }: AnimateProps) => {
-//   const path = usePathname();
-//   return (
-//     <AnimatePresence mode="wait">
-//       <motion.div
-//         initial={{ x: "-100vw" }}
-//         animate={{ x: 0 }}
-//         transition={{ ease: "linear", delay: 0.5 }}
-//       >
-//         {children}
-//       </motion.div>
-//     </AnimatePresence>
-//   );
-// };
-
 export const AnimateContainer = ({ children }: AnimateProps) => {
-  const PageTransition = {
-    async leave() {
-      // Code to animate the leaving page, e.g., fading out
-      gsap.to("#body", { duration: 0.5, opacity: 0 });
-    },
-
-    async enter() {
-      // Code to animate the entering page, e.g., fading in
-      gsap.to("#body", { duration: 1, opacity: 0 });
-
-      console.log("Entering page...");
-    },
-  };
-
-  useEffect(() => {
-    if (!barba) return;
-
-    barba.init({
-      transitions: [PageTransition],
-      debug: true,
-    });
-
-    return () => {
-      barba.destroy();
-    };
-  }, [barba]);
-
-  return (
-    <div data-barba="wrapper">
-      <div data-barba="container">{children}</div>
-    </div>
-  );
+  const path = usePathname();
+  return <div key={path}>{children}</div>;
 };
