2ff401af1510fab8f4434b35898f870034a89d7b
Serotoninene <pujol.alexandre@hotmail.fr>
2023-07-31 18:47:40 +0200

    integrated the mouse position to the shder

--- src/shaders/HomePhotoShader/vertex.glsl
+++ src/shaders/HomePhotoShader/vertex.glsl
@@ -2,10 +2,12 @@
   // uniform mat4 viewMatrix;
   // uniform mat4 modelMatrix;
 
+  uniform vec2 uMouse;
+
   varying vec2 vUv;
 
   void main()
   {
-      vUv = uv;
+      vUv = uv + uMouse;
       gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
   }
