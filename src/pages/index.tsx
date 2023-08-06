579f44446dad59f5e248f6d5cf7517d7ce834511
Serotoninene <pujol.alexandre@hotmail.fr>
2023-08-06 18:14:34 +0200

    swithced photos on the home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -86,6 +86,16 @@ const photos = [
     },
   },
   {
+    ...createPhoto("/assets/photos/08_ALL_ABOUT_CLEANING.jpeg"),
+    dominantColor: "#c3afa5",
+    caption: {
+      idx: "",
+      title: "ALL ABOUT CLEANING",
+      place: "Canary Islands (ES)",
+      date: "2023",
+    },
+  },
+  {
     ...createPhoto("/assets/photos/09_SUPERMARKET.jpeg"),
     dominantColor: "#9e382f",
     caption: {
@@ -93,16 +103,6 @@ const photos = [
       title: "SUPERMARKET",
       place: "Provence-Alpes-Côte d’Azur (FR)",
       date: "2021",
-    },
-  },
-  {
-    ...createPhoto("/assets/photos/10_TIME_FOR_LAUNDRY.jpeg"),
-    dominantColor: "#a8a4aa",
-    caption: {
-      idx: "10",
-      title: "TIME FOR LAUNDRY",
-      place: "Vancouver (CA)",
-      date: "2020",
     },
   },
   {
@@ -116,25 +116,26 @@ const photos = [
     },
   },
   {
-    ...createPhoto("/assets/photos/12_AROUND_THE_CORNER.jpeg"),
-    dominantColor: "#ababa7",
+    ...createPhoto("/assets/photos/10_TIME_FOR_LAUNDRY.jpeg"),
+    dominantColor: "#a8a4aa",
     caption: {
-      idx: "12",
-      title: "AROUND THE CORNER",
-      place: "Canary Islands (ES)",
-      date: "2021",
-    },
-  },
-  {
-    ...createPhoto("/assets/photos/13_TROPICAL_CHURCH.jpeg"),
-    dominantColor: "#aeaab9",
-    caption: {
-      idx: "13",
-      title: "TROPICAL CHURCH",
-      place: "Honolulu (US)",
+      idx: "10",
+      title: "TIME FOR LAUNDRY",
+      place: "Vancouver (CA)",
       date: "2020",
     },
   },
+  {
+    ...createPhoto("/assets/photos/31_LIFE_PERSPECTIVES.jpeg"),
+    dominantColor: "#ababa7",
+    caption: {
+      idx: "12",
+      title: "LIFE PERSPECTIVES",
+      place: "Vancouver Island (CA)",
+      date: "2020",
+    },
+  },
+
   {
     ...createPhoto("/assets/photos/14_ROOMS_FOR_ME_&_FOR_MY_CAR.jpeg"),
     dominantColor: "#cfbaa5",
@@ -156,53 +157,145 @@ const photos = [
     },
   },
   {
-    ...createPhoto("/assets/photos/08_ALL_ABOUT_CLEANING.jpeg"),
-    dominantColor: "#c3afa5",
+    ...createPhoto("/assets/photos/13_TROPICAL_CHURCH.jpeg"),
+    dominantColor: "#aeaab9",
     caption: {
-      idx: "",
-      title: "ALL ABOUT CLEANING",
-      place: "Canary Islands (ES)",
-      date: "2023",
-    },
-  },
-  {
-    ...createPhoto("/assets/photos/16_WHITE_IN_THE_BLUE.jpeg"),
-    dominantColor: "#cabdb7",
-    caption: {
-      idx: "16",
-      title: "WHITE IN THE BLUE",
-      place: "Madrid (ES)",
-      date: "2021",
-    },
-  },
-  {
-    ...createPhoto("/assets/photos/17_A_POLAR_CHURCH.jpeg"),
-    dominantColor: "#262b33",
-    caption: {
-      idx: "17",
-      title: "A POLAR CHURCH",
-      place: "Yukon (CA)",
-      date: "2020",
-    },
-  },
-  {
-    ...createPhoto("/assets/photos/18_A_SCHOOL_IN_COLOR.jpeg"),
-    dominantColor: "#304d4e",
-    caption: {
-      idx: "18",
-      title: "A SCHOOL IN COLOR",
-      place: "Victoria (CA)",
+      idx: "13",
+      title: "TROPICAL CHURCH",
+      place: "Honolulu (US)",
       date: "2020",
     },
   },
   {
     ...createPhoto("/assets/photos/19_I_MISSED_THE_TRAIN.jpeg"),
-    dominantColor: "#a39279",
+    dominantColor: "#cabdb7",
     caption: {
-      idx: "19",
+      idx: "16",
       title: "I MISSED THE TRAIN",
       place: "Somewhere in Norway (NO)",
       date: "2022",
+    },
+  },
+  {
+    ...createPhoto("/assets/photos/23_GOING_OUT_FOR_DINNER.jpeg"),
+    dominantColor: "#262b33",
+    caption: {
+      idx: "17",
+      title: "GOING OUT FOR DINNER",
+      place: "Alberta (CA)",
+      date: "2020",
+    },
+  },
+  {
+    ...createPhoto("/assets/photos/24_DOWN_BY_THE_LAKE.jpeg"),
+    dominantColor: "#304d4e",
+    caption: {
+      idx: "18",
+      title: "DOWN BY THE LAKE",
+      place: "Vancouver Island (CA)",
+      date: "2020",
+    },
+  },
+  {
+    ...createPhoto("/assets/photos/25_THROUGH_THE_MIRROR.jpeg"),
+    dominantColor: "#a39279",
+    caption: {
+      idx: "19",
+      title: "THROUGH THE MIRROR",
+      place: "Mexico City (MX)",
+      date: "2019",
+    },
+  },
+  {
+    ...createPhoto("/assets/photos/42_TIME_TO_DRY.jpeg"),
+    dominantColor: "",
+    caption: {
+      idx: "42",
+      title: "TIME TO DRY",
+      place: "Saint-Tropez (FR)",
+      date: "2022",
+    },
+  },
+  {
+    ...createPhoto("/assets/photos/43_PRIMAVERA.jpeg"),
+    dominantColor: "",
+    caption: {
+      idx: "43",
+      title: "PRIMAVERA",
+      place: "Bruxelles (BE)",
+      date: "2022",
+    },
+  },
+
+  {
+    ...createPhoto("/assets/photos/44_LEMONADE.jpeg"),
+    dominantColor: "#d2c6b4",
+    caption: {
+      idx: "22",
+      title: "LEMONADE",
+      place: "Amsterdam (NL)",
+      date: "2022",
+    },
+  },
+  {
+    ...createPhoto("/assets/photos/72_LA_VIE_ET_SES_PLAISIRS.jpeg"),
+    dominantColor: "#789aa8",
+    caption: {
+      idx: "23",
+      title: "LA VIE ET SES PLAISIRS",
+      place: "Saint-Tropez (FR)",
+      date: "2022",
+    },
+  },
+  {
+    ...createPhoto("/assets/photos/71_THERE_IS_NOTHING_ON_THE_ROOF.jpeg"),
+    dominantColor: "#817250",
+    caption: {
+      idx: "24",
+      title: "THERE IS NOTHING ON THE ROOF",
+      place: "Bergen (NO)",
+      date: "2022",
+    },
+  },
+  {
+    ...createPhoto("/assets/photos/45_LIVING_THE_DREAM.jpeg"),
+    dominantColor: "#ba9a48",
+    caption: {
+      idx: "25",
+      title: "LIVING THE DREAM",
+      place: "Hawaï (US)",
+      date: "2020",
+    },
+  },
+  {
+    ...createPhoto("/assets/photos/46_DREAMING_THE_DREAM.jpeg"),
+    dominantColor: "#688fa1",
+    caption: {
+      idx: "26",
+      title: "DREAMING THE DREAM",
+      place: "Alberta (CA)",
+      date: "2020",
+    },
+  },
+  {
+    ...createPhoto("/assets/photos/48_LA_FILLE_ET_LE_PARAPLUIE.jpeg"),
+    dominantColor: "#19261d",
+    caption: {
+      idx: "27",
+      title: "LA FILLE ET LE PARAPLUIE",
+      place: "Paris (FR)",
+      date: "2022",
+    },
+  },
+  // 2020, Victoria (CA).
+  {
+    ...createPhoto("/assets/photos/47_LE_PIED_ET_LE_PARASOL.jpeg"),
+    dominantColor: "#decfc0",
+    caption: {
+      idx: "28",
+      title: "LE PIED ET LE PARASOL",
+      place: "Victoria (CA)",
+      date: "2020",
     },
   },
   {
@@ -223,96 +316,6 @@ const photos = [
       title: "LOVE AT THE STREET CORNER",
       place: "Brussels (BE)",
       date: "2022",
-    },
-  },
-  {
-    ...createPhoto("/assets/photos/22_HIDING_FROM_THE_SHADOWS.jpeg"),
-    dominantColor: "#d2c6b4",
-    caption: {
-      idx: "22",
-      title: "HIDING FROM THE SHADOWS",
-      place: "Honolulu (US)",
-      date: "2020",
-    },
-  },
-  {
-    ...createPhoto("/assets/photos/23_GOING_OUT_FOR_DINNER.jpeg"),
-    dominantColor: "#789aa8",
-    caption: {
-      idx: "23",
-      title: "GOING OUT FOR DINNER",
-      place: "Alberta (CA)",
-      date: "2020",
-    },
-  },
-  {
-    ...createPhoto("/assets/photos/24_DOWN_BY_THE_LAKE.jpeg"),
-    dominantColor: "#817250",
-    caption: {
-      idx: "24",
-      title: "DOWN BY THE LAKE",
-      place: "Vancouver Island (CA)",
-      date: "2020",
-    },
-  },
-  {
-    ...createPhoto("/assets/photos/25_THROUGH_THE_MIRROR.jpeg"),
-    dominantColor: "#ba9a48",
-    caption: {
-      idx: "25",
-      title: "THROUGH THE MIRROR",
-      place: "Mexico City (MX)",
-      date: "2019",
-    },
-  },
-  {
-    ...createPhoto("/assets/photos/26_REST_AREA.jpeg"),
-    dominantColor: "#688fa1",
-    caption: {
-      idx: "26",
-      title: "REST AREA",
-      place: "Alberta (CA)",
-      date: "2020",
-    },
-  },
-  {
-    ...createPhoto("/assets/photos/27_THE_PERFECT_FAMILY.jpeg"),
-    dominantColor: "#19261d",
-    caption: {
-      idx: "27",
-      title: "THE PERFECT FAMILY",
-      place: "Lillehammer (NO)",
-      date: "2022",
-    },
-  },
-  {
-    ...createPhoto("/assets/photos/28_FUMER_TUE.jpeg"),
-    dominantColor: "#decfc0",
-    caption: {
-      idx: "28",
-      title: "FUMER TUE",
-      place: "Brussels (BE)",
-      date: "2022",
-    },
-  },
-  {
-    ...createPhoto("/assets/photos/29_UNE_MAQUETTE.jpeg"),
-    dominantColor: "#2f351c",
-    caption: {
-      idx: "29",
-      title: "UNE MAQUETTE",
-      place: "Montréal (CA)",
-      date: "2019",
-    },
-  },
-  {
-    ...createPhoto("/assets/photos/30_DIVING_IN_THE_BLUE.jpeg"),
-    dominantColor: "#c8c9d4",
-    caption: {
-      idx: "30",
-      title: "DIVING IN THE BLUE",
-      place: "Canary Islands (ES)",
-      date: "2021",
     },
   },
   {
@@ -386,14 +389,14 @@ export default function Home() {
             secondPhoto={photos[10]}
             inverted
           />
-          <OneWoCaption photo={photos[10]} />
+          <OneWoCaption photo={photos[11]} />
           <GridThree
-            firstPhoto={photos[13]}
-            secondPhoto={photos[15]}
+            firstPhoto={photos[12]}
+            secondPhoto={photos[13]}
             thirdPhoto={photos[14]}
             inverted
           />
-          <OneCentered photo={photos[7]} />
+          <OneCentered photo={photos[15]} />
           <GridThree
             firstPhoto={photos[16]}
             secondPhoto={photos[17]}
@@ -402,8 +405,8 @@ export default function Home() {
           <GridTwo firstPhoto={photos[19]} secondPhoto={photos[20]} />
           <OneWCaption photo={photos[21]}>
             <>
-              <span className="font-bold">Embracing</span> the sunny days{" "}
-              <span className="font-bold">really</span>.
+              <span className="font-bold">The couch</span> couch waits all day
+              for you <span className="font-bold">to come home.</span>
             </>
           </OneWCaption>
           <TwoShifted
