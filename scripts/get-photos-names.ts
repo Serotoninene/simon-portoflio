6e90d2ae3311d370866b53516587e23320e868cd
Serotoninene <pujol.alexandre@hotmail.fr>
2023-08-09 16:13:07 +0200

    made a script to get all the names of the photos in an array

---
+++ scripts/get-photos-names.ts
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+
+const directoryPath = path.join(__dirname, "../public/assets/photos"); // Adjust the path as needed
+
+const fetchPhotoFileNames = () => {
+  try {
+    const fileNames = fs.readdirSync(directoryPath);
+    console.log(JSON.stringify(fileNames));
+  } catch (error) {
+    console.error("Error fetching photo file names:", error);
+  }
+};
+
+fetchPhotoFileNames();
