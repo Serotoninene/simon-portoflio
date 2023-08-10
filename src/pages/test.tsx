000852afd3d0e518a20a0360f082eac7310045da
Serotoninene <pujol.alexandre@hotmail.fr>
2023-08-10 15:59:39 +0200

    made the flip animation work

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,17 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 import { photos } from "./work";
 import Image from "next/image";
-import { gsap } from "gsap";
+import { gsap, Power3 } from "gsap";
 import { Flip } from "gsap/dist/Flip";
 
 type Props = {};
 
-const GalleryPhoto = ({ photo, galleryState }: any) => {
+const GalleryPhoto = ({ idx, photo, photosRef, galleryState }: any) => {
   // GRID STATE
   if (galleryState === "grid")
     return (
       <div
+        ref={(e) => (photosRef.current[idx] = e)}
         data-flip-id={photo.alt}
         className="gallery-photo p-2 relative h-[50px] overflow-hidden"
       >
@@ -31,31 +32,40 @@ const GalleryPhoto = ({ photo, galleryState }: any) => {
 };
 
 export default function Test(props: Props) {
+  const photosRef = useRef<any[]>([]);
+  const [flipState, setFlipState] = useState<any>();
   const [galleryState, setGalleryState] = useState<"grid" | "flex">("grid");
   const toggleState = () => {
-    const state = Flip.getState(".gallery-photo");
+    setFlipState(Flip.getState(".gallery-photo"));
     setGalleryState(galleryState === "grid" ? "flex" : "grid");
-    Flip.from(state, {
-      duration: 2,
-      absolute: true,
-    });
   };
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     gsap.registerPlugin(Flip);
 
-    return () => {
-      gsap.killTweensOf(Flip);
-    };
-  }, []);
+    if (!flipState) return;
+
+    Flip.from(flipState, {
+      stagger: {
+        amount: 0.1,
+        from: galleryState === "grid" ? "start" : "end",
+      },
+      // staggerFrom: galleryState === "grid" ? "start" : "end",
+      ease: Power3.easeOut,
+      absolute: true,
+    });
+  }, [galleryState, flipState]);
+
   return (
     <div
       id=".gallery-container"
       className={`${galleryState} grid-cols-4 flex-col gap-10`}
     >
-      {photos.map((photo) => (
+      {photos.map((photo, idx) => (
         <GalleryPhoto
+          idx={idx}
           photo={photo}
+          photosRef={photosRef}
           galleryState={galleryState}
           key={photo.alt}
         />
