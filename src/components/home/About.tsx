b1bc089ba1b24a9f9f8115d6aad3233c35ffd223
Serotoninene <pujol.alexandre@hotmail.fr>
2023-08-22 17:20:54 +0200

    triggering the effect when in view

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { AnimatedWords } from "../atoms";
+import { useInView } from "framer-motion";
 
 type Props = {};
 
 export const About = (props: Props) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
   return (
-    <div className="grid gap-8 md:grid-cols-8 ">
+    <div ref={ref} className="grid gap-8 md:grid-cols-8 ">
       <div className="col-span-3 font-black text-4xl leading-[150%]">
         <AnimatedWords
+          stagger={0.01}
+          start={isInView}
+          fontWeight="font-bold"
           string="What is supposed to be photogenic does not interest me as much as its
         inherent beauty."
-          fontWeight="font-black"
-          stagger={0.02}
         />
       </div>
       <div />
 
-      <div className="col-span-2 font-medium text-justify flex items-end sm:text-lg">
-        {" "}
-        It is more intriguing to me to offer a unique and original composition
-        of something that is ultimately ordinary.{" "}
+      <div className="col-span-2 text-justify flex items-end sm:text-lg">
+        <AnimatedWords
+          stagger={0.005}
+          start={isInView}
+          delay={0.3}
+          fontWeight="font-medium"
+          string="It is more intriguing to me to offer a unique and original composition
+          of something that is ultimately ordinary."
+        />
       </div>
-      <div className="col-span-2 font-medium text-justify flex items-end sm:text-lg">
-        Without pretension, I would like to pay tribute here to all objects, all
-        bodies, all gestures, all buildings, and all the details that have
-        inspired me at the moment when we crossed paths somewhere.
+      <div className="col-span-2 text-justify flex items-end sm:text-lg">
+        <AnimatedWords
+          stagger={0.005}
+          start={isInView}
+          delay={0.5}
+          fontWeight="font-medium"
+          string="Without pretension, I would like to pay tribute here to all objects, all
+          bodies, all gestures, all buildings, and all the details that have
+          inspired me at the moment when we crossed paths somewhere."
+        />
       </div>
     </div>
   );
