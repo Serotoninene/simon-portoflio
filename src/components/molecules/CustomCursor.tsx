7f40f72cfa1f218e109d3db4814e8e5f8175e17f
Serotoninene <pujol.alexandre@hotmail.fr>
2023-09-04 18:10:14 +0200

    made the cursor following the cursor without using framer motion - too demanding in performance

--- src/components/molecules/CustomCursor.tsx
+++ src/components/molecules/CustomCursor.tsx
@@ -1,27 +1,33 @@
-import { useEffect } from "react";
-import { motion, useSpring } from "framer-motion";
+import { useEffect, useRef } from "react";
+import { motion } from "framer-motion";
+
 // Context
 import { useCursorContext } from "@/context/CursorContext";
 import AnimatedLetters from "../atoms/AnimLetters";
 
 export default function CustomCursor() {
+  const cursorRef = useRef<HTMLDivElement>(null);
   const { cursorType } = useCursorContext();
 
-  const springConfig = {
-    stiffness: 500,
-    damping: 15,
-    mass: 0.5,
-  };
+  // COMMENTED ALL THE LOGIC ANIMATING THE CURSOR WITH FRAMER MOTION -- TOO DEMANDING IN PERFORMANCE BUT PERHAPS IMPROVABLE
+  // const springConfig = {
+  //   stiffness: 500,
+  //   damping: 15,
+  //   mass: 0.5,
+  // };
 
-  const mousePosition = {
-    x: useSpring(200, springConfig),
-    y: useSpring(200, springConfig),
-  };
+  // const mousePosition = {
+  //   x: useSpring(200, springConfig),
+  //   y: useSpring(200, springConfig),
+  // };
 
   const onMouseMove = (event: MouseEvent) => {
+    if (!cursorRef.current) return;
     const { clientX, clientY } = event;
-    mousePosition.x.set(clientX - 10);
-    mousePosition.y.set(clientY - 10);
+    cursorRef.current.style.top = `${clientY - 10}px`;
+    cursorRef.current.style.left = `${clientX - 10}px`;
+    // mousePosition.x.set(clientX - 10);
+    // mousePosition.y.set(clientY - 10);
   };
 
   useEffect(() => {
@@ -35,10 +41,11 @@ export default function CustomCursor() {
   return (
     <motion.div
       id="CustomCursor" // had to use css for styling here --> base.scss
+      ref={cursorRef}
       className={cursorType}
       style={{
-        x: mousePosition.x,
-        y: mousePosition.y,
+        // x: mousePosition.x,
+        // y: mousePosition.y,
         scale: cursorType === "hover" ? 0.5 : 1,
       }}
     >
