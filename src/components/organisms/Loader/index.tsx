0af5e551423122f0e11135a70cc62b93753a28c4
Serotoninene <pujol.alexandre@hotmail.fr>
2023-09-07 16:35:57 +0200

    adjusting the loading animation

--- src/components/organisms/Loader/index.tsx
+++ src/components/organisms/Loader/index.tsx
@@ -34,7 +34,7 @@ export const Loader = () => {
     <div className="h-[var(--fullScreen)] w-screen flex justify-center items-center fixed top-0 left-0 bg-light z-20 overflow-hidden text-3xl font-black">
       <svg
         viewBox="0 0 56 10" // Set the viewBox to match the original dimensions
-        width="50vw"
+        width="20vw"
       >
         <defs>
           {loadingPath.map((letter, idx) => (
@@ -50,27 +50,24 @@ export const Loader = () => {
             clip-path={`url(#clip-path${idx})`}
             initial={{
               pathLength: 0,
-              pathOffset: 0.1,
               opacity: 0,
             }}
             animate={{
               pathLength: [0, 1, 0],
-              strokeWidth: [1, 6, 1],
+              strokeWidth: [1, 50, 0],
               pathOffset: [0.1, 0, 0.1],
               opacity: [0, 1, 0],
             }}
             transition={{
               delay: 0.5 + idx * 0.1,
               ease: "easeInOut",
-
-              duration: 5,
+              duration: 3.5,
               repeat: Infinity,
               repeatType: "loop",
             }}
             d={letter}
             id={"path" + idx}
             stroke="#071732"
-            strokeWidth={0}
             strokeLinejoin={"round"}
             strokeLinecap={"round"}
             fill="none"
