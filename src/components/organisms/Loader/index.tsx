656e7b8e312710970c27c0f9b89d0a2244264b93
Serotoninene <pujol.alexandre@hotmail.fr>
2023-09-07 17:57:50 +0200

    made an exit anim for the loader

--- src/components/organisms/Loader/index.tsx
+++ src/components/organisms/Loader/index.tsx
@@ -5,6 +5,16 @@ import { useLocomotiveScroll } from "react-locomotive-scroll";
 import { useLoadingContext } from "@/context/LoadingContext";
 import { loadingPath } from "./paths";
 
+const containerAnim = {
+  exit: {
+    y: "100%",
+    transition: {
+      delay: 0.7,
+      ease: "easeOut",
+    },
+  },
+};
+
 const drawAnimation: Variants = {
   hidden: { pathLength: 0, opacity: 0 },
   visible: (custom: number) => ({
@@ -20,62 +30,78 @@ const drawAnimation: Variants = {
       repeatType: "loop",
     },
   }),
+
+  exit: (custom: number) => ({
+    y: "200%",
+    transition: {
+      delay: custom * 0.02,
+      ease: "easeIn",
+      duration: 0.5,
+    },
+  }),
 };
 
 export const Loader = () => {
-  const { isLoaded } = useLoadingContext();
-  const [isLoaderShown, setIsLoaderShown] = useState(true);
   const { scroll } = useLocomotiveScroll();
-  !isLoaded && scroll?.stop();
+  const { isLoaded } = useLoadingContext();
+  const [isExitAnimationComplete, setIsExitAnimationComplete] = useState(false);
 
+  !isLoaded && scroll?.stop();
   useEffect(() => {
     if (isLoaded) {
       scroll?.start();
     }
   }, [isLoaded]);
 
-  // if (!isLoaderShown) return null;
+  const unMountAfterExitAnim = () => {
+    setIsExitAnimationComplete(true);
+  };
+
+  // if the exit animation is complete or if the loading has already been done, return null
+  // if (isExitAnimationComplete || isLoaded) return null;
 
   return (
     <AnimatePresence mode="wait">
-      <motion.div
-        exit={{ y: "100vh" }}
-        onAnimationComplete={() => {
-          console.log();
-        }}
-        key={isLoaded.toString()}
-        className="h-[var(--fullScreen)]  w-screen flex justify-center items-center fixed top-0 left-0 bg-light z-20 overflow-hidden text-3xl font-black"
-      >
-        <svg
-          viewBox="0 0 56 10" // Set the viewBox to match the original dimensions
-          width="20vw"
+      {isExitAnimationComplete || isLoaded ? null : (
+        <motion.div
+          variants={containerAnim}
+          exit="exit"
+          onAnimationComplete={unMountAfterExitAnim}
+          key={isLoaded.toString()}
+          className="h-[var(--fullScreen)]  w-screen flex justify-center items-center fixed top-0 left-0 bg-light z-20 overflow-hidden text-3xl font-black"
         >
-          <defs>
-            {loadingPath.map((letter, idx) => (
-              <clipPath key={letter} id={`clip-path${idx}`} d={letter}>
-                <path d={letter} fill="none" />
-              </clipPath>
-            ))}
-          </defs>
+          <svg
+            viewBox="0 0 56 10" // Set the viewBox to match the original dimensions
+            width="20vw"
+          >
+            <defs>
+              {loadingPath.map((letter, idx) => (
+                <clipPath key={letter} id={`clip-path${idx}`} d={letter}>
+                  <path d={letter} fill="none" />
+                </clipPath>
+              ))}
+            </defs>
 
-          {loadingPath.map((letter, idx) => (
-            <motion.path
-              key={`${letter}path`}
-              clipPath={`url(#clip-path${idx})`}
-              variants={drawAnimation}
-              custom={idx}
-              initial="hidden"
-              animate="visible"
-              d={letter}
-              id={"path" + idx}
-              stroke="#071732"
-              strokeLinejoin={"round"}
-              strokeLinecap={"round"}
-              fill="none"
-            />
-          ))}
-        </svg>
-      </motion.div>
+            {loadingPath.map((letter, idx) => (
+              <motion.path
+                key={`${letter}path`}
+                clipPath={`url(#clip-path${idx})`}
+                variants={drawAnimation}
+                custom={idx}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                d={letter}
+                id={"path" + idx}
+                stroke="#071732"
+                strokeLinejoin={"round"}
+                strokeLinecap={"round"}
+                fill="none"
+              />
+            ))}
+          </svg>
+        </motion.div>
+      )}
     </AnimatePresence>
   );
 };
