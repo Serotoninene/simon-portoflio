a786238701b9434bff2963c869cf190cbd8ab617
Serotoninene <pujol.alexandre@hotmail.fr>
2023-09-07 16:10:37 +0200

    animating the loading

--- src/components/molecules/Layout.tsx
+++ src/components/molecules/Layout.tsx
@@ -10,7 +10,7 @@ import { useWindowSize } from "@/utils/hooks";
 import CustomCursor from "./CustomCursor";
 import { LoadingProvider } from "@/context/LoadingContext";
 
-const poppins = Poppins({
+export const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
--- src/components/organisms/Loader/index.tsx
+++ src/components/organisms/Loader/index.tsx
@@ -1,8 +1,22 @@
 import React, { useEffect } from "react";
-import { useLoadingContext } from "@/context/LoadingContext";
+import { motion } from "framer-motion";
 import { useLocomotiveScroll } from "react-locomotive-scroll";
+
+import { useLoadingContext } from "@/context/LoadingContext";
 import { loadingPath } from "./paths";
 
+const draw = {
+  hidden: { opacity: 0, strokeDashOffset: 0 },
+  visible: {
+    strokeDashoffset: 200,
+    opacity: 1,
+
+    transition: {
+      pathLength: { type: "spring", duration: 1.5, bounce: 0 },
+      opacity: { duration: 0.01 },
+    },
+  },
+};
 export const Loader = () => {
   const { isLoaded } = useLoadingContext();
   const { scroll } = useLocomotiveScroll();
@@ -20,10 +34,47 @@ export const Loader = () => {
     <div className="h-[var(--fullScreen)] w-screen flex justify-center items-center fixed top-0 left-0 bg-light z-20 overflow-hidden text-3xl font-black">
       <svg
         viewBox="0 0 56 10" // Set the viewBox to match the original dimensions
-        width="20vw"
+        width="50vw"
       >
+        <defs>
+          {loadingPath.map((letter, idx) => (
+            <clipPath key={letter} id={`clip-path${idx}`} d={letter}>
+              <path d={letter} fill="none" />
+            </clipPath>
+          ))}
+        </defs>
+
         {loadingPath.map((letter, idx) => (
-          <path key={letter} d={letter} />
+          <motion.path
+            key={`${letter}path`}
+            clip-path={`url(#clip-path${idx})`}
+            initial={{
+              pathLength: 0,
+              pathOffset: 0.1,
+              opacity: 0,
+            }}
+            animate={{
+              pathLength: [0, 1, 0],
+              strokeWidth: [1, 6, 1],
+              pathOffset: [0.1, 0, 0.1],
+              opacity: [0, 1, 0],
+            }}
+            transition={{
+              delay: 0.5 + idx * 0.1,
+              ease: "easeInOut",
+
+              duration: 5,
+              repeat: Infinity,
+              repeatType: "loop",
+            }}
+            d={letter}
+            id={"path" + idx}
+            stroke="#071732"
+            strokeWidth={0}
+            strokeLinejoin={"round"}
+            strokeLinecap={"round"}
+            fill="none"
+          />
         ))}
       </svg>
     </div>
